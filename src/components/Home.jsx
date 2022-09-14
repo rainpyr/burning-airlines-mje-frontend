5c7bb0f0aae425dae44d60143d1e688d7c3785da
Mohamad Dakdouk <mdak2806@gmail.com>
2022-09-14 13:41:30 +1000

    users

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
-import Reservations from './components/Reservations'
-import Users from './components/Users'
+import React from 'react';
+import Reservations from './Reservations';
+import Users from './Users';
+
+
+// import {Route, HashRouter as Router, Link} from 'react-router-dom';
+
 
 class Home extends React.Component {
 
@@ -8,24 +12,32 @@ class Home extends React.Component {
 
         return (
             <div className="App">
-                <Router>
+                {/* <Router> */}
                 <header>
                 <h1>WELCOME TO MJE EXCLUSIVE AIRLINES!</h1>
                 <p>We only run exclusive jets so our seats are little. Not for poor people</p>
                 <hr />
                     <nav>
-                        <Link to="/">Home</Link>
+                        {/* <Link to='/'>Home</Link>
                         {' '}|{' '}
+                        <Link to='/reservations'>Reservations</Link>
+                        {' '}|{' '}
+                        <Link to='/users'>Users</Link> */}
+                        <Users />
+                        <Reservations />
+
                     </nav>
                     <hr />
                 </header>
+                {/* <Route exact path="/users" component={Users} />
+                <Route exact path="/reservations" component={ Reservations }/> */}
 
                
 
-                </Router>
+                {/* </Router> */}
             </div>
         )
     }
 }
 
-export default Home
+export default Home;
