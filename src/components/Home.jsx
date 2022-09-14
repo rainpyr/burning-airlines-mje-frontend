39015c0c8893af1bfaa5e50fc05689347cb10a24
Justin Tan <justintws10@gmail.com>
2022-09-14 11:00:52 +1000

    added home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Home extends React.Component {
+
+    render(){
+
+        return (
+            <div className="App">
+                <h1>WELCOME TO MJE EXCLUSIVE AIRLINES!</h1>
+                <p>We only run exclusive jets so our seats are little. Not for poor people</p>
+            </div>
+        )
+    }
+}
+
+export default Home
